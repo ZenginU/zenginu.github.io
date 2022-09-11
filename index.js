b276e9bc692069b1f1550ec08af6731395f12a77
ZenginU <umutzengin00@gmail.com>
2022-09-11 18:22:42 +0300

    Added Site Nav and JS

---
+++ index.js
@@ -0,0 +1,10 @@
+function addFooter() {
+  document.body.innerHTML += `
+  <footer class="container">
+    <ul>
+      <li><a href="https://www.linkedin.com/in/zenginumut/" target="popup">LinkedIn</a></li>
+      <li><a href="https://github.com/ZenginU" target="popup">GitHub</a></li>
+      <li><a href="mailto:umutzengin00@gmail.com" target="popup">Mail</a></li>
+    </ul>
+  </footer>`;
+}
